ea98d69bd17fc707bcddb431c3e7410602eb3a25
unknown <202080469@psu.palawan.edu.ph>
2022-09-19 23:56:59 +0800

    done web5

--- web5/index.php
+++ web5/index.php
@@ -1,7 +1,31 @@
 <?php
+	session_start();	
+    
+	if(isset($_SESSION['views']))
+	{
+   	 	$_SESSION['views'] = $_SESSION['views']+1;
+	}
+	
+	else
+	{
+    	$_SESSION['views']=1;
+	}
+	$num_visits = $_SESSION['views'];
 
 ?>
-
 <h1>Hello</h1>
 <p>You have visited this page <?= $num_visits ?> times</p>
 <p><a href="?reset=true">Reset</a></p>
+
+<?php  
+
+	if($num_visits == 5)
+   	{
+   		echo "<p> <b>WOW!!!you are visiting a lot. Thank you</b> for visiting the page a lot.</p>";
+   	}
+
+   	elseif ($num_visits == 10) 
+   	{
+   		 header('Location: congratulations.php');
+   	}
+?>
