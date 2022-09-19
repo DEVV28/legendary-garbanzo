f76684d48663f127e6193e74d0ed4755181dfd5a
unknown <202080469@psu.palawan.edu.ph>
2022-09-19 22:00:17 +0800

    done web3

--- web3/calc.php
+++ web3/calc.php
@@ -2,18 +2,35 @@
 <head><title>Calculation Result</title></head>
 <body>
     <?php
-        # test if input is_numeric
-        if(1){
-            # test if calc is not null
-            if(1){
-                # switch statement for value of calc
-                switch(1){
-                    
-                }
-                echo "Calculation result: $result";
-            }
-        }
-        else{ echo "Invalid entry - please retry"; }
-    ?>
+              # test if input is_numeric
+              $val1 = $_POST['val1'];
+              $val2 = $_POST['val2'];
+      
+              if (is_numeric($val1) && is_numeric($val2)){
+                  
+                  # test if calc is not null
+                  if(isset($_POST['calc'])){
+                      $calc = $_POST['calc'];
+                      
+                      # switch statement for value of calc    
+                      switch($calc){
+                          case "add":
+                              $result = $val1 + $val2;
+                               break;
+                          case "sub":
+                              $result = $val1 - $val2;
+                               break;
+                          case "mul":
+                              $result = $val1 * $val2;
+                               break;
+                          case "div":
+                              $result = $val1 / $val2;
+                          
+                      }
+                      echo "Calculation result: $result";
+                  }
+              }
+              else{ echo "Invalid entry - please retry"; }
+          ?>
 </body>
 </html>
