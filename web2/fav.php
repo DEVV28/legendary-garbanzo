73314d9efe5578a1ce004429c2ca26940a14972e
Sue-Sinangote_ <111571821+SueSinangote@users.noreply.github.com>
2022-09-19 20:41:10 +0800

    Update fav.php

--- web2/fav.php
+++ web2/fav.php
@@ -2,7 +2,44 @@
 <head><title>Your Favorite</title></head>
 <body>
     <?php
+<<<<<<< HEAD
+       
+        if(isset($_POST['Username'])){
+
+            $user = $_POST['Hunter'];
+            $Selections = empty($User);
+           
+            $W = $_POST['Weapons']; 
+            $class = empty($b);
+
         
+                if ($selections != 1){       
+                    if(isset($_POST['color']) & $class != 1){
+                        $elmt = $_POST['color'];              
+                        echo "Good choice!, A Fine Weapon of your Class, Very well I hope it serve you well! ".$user. "<br>hmm, Interesting!! ". $b . " <br>- With an Elemantal Enchancement it will be formidable" . $a . " with this you are Unstoppable!";
+
+                }else {echo "Ah!, A Bow and arrow favored by the skilled, few!."; }
+
+
+                }else {echo "What is your Name,Hunter."; }
+
+          }
     ?>
+    
+=======
+        if(isset($_POST['Username'])){
+        echo $_POST['Username'];
+        }
+
+        if(isset($_POST['color'])){
+            if (isset($_POST['color'=='Fire'])){
+                
+            }
+        }
+
+
+        ?>
+
+>>>>>>> 4756803dcbb36238958acd637ed831e40a2b1e58
 </body>
 </html>
