6e8e213141af9972a281a20084f6e3e68b024a71
Sue-Sinangote_ <111571821+SueSinangote@users.noreply.github.com>
2022-09-19 20:59:27 +0800

    Update fav.php

--- web2/fav.php
+++ web2/fav.php
@@ -2,44 +2,43 @@
 <head><title>Your Favorite</title></head>
 <body>
     <?php
-<<<<<<< HEAD
+<<<<<<< HEADING
        
-        if(isset($_POST['Username'])){
-
-            $user = $_POST['Hunter'];
-            $Selections = empty($User);
-           
-            $W = $_POST['Weapons']; 
+        if(isset($_POST['Hunter'])){
+            $hunter = $_POST['hunter'];
+             $W = $_POST['Weapons']; 
+            
+            $Selections = empty($Hunter);
             $class = empty($b);
 
-        
-                if ($selections != 1){       
-                    if(isset($_POST['color']) & $class != 1){
+        /////
+             if ($selections != 1){ 
+                 
+              if(isset($_POST['color']) & $class != 1){
                         $elmt = $_POST['color'];              
-                        echo "Good choice!, A Fine Weapon of your Class, Very well I hope it serve you well! ".$user. "<br>hmm, Interesting!! ". $b . " <br>- With an Elemantal Enchancement it will be formidable" . $a . " with this you are Unstoppable!";
+         echo "Good choice!, A Fine Weapon of your Class, Very well I hope it serve you well! "
+             .$user. "<br>hmm, Interesting!! ". 
+             $b . " <br>- With an Elemantal Enchancement it will be formidable" . 
+             $a . " with this you are Unstoppable!";
 
                 }else {echo "Ah!, A Bow and arrow favored by the skilled, few!."; }
-
 
                 }else {echo "What is your Name,Hunter."; }
 
           }
     ?>
-    
-=======
-        if(isset($_POST['Username'])){
-        echo $_POST['Username'];
+    ////////////
+        if(isset($_POST['Hunter'])){
+        echo $_POST['Hunter'];
         }
-
         if(isset($_POST['color'])){
-            if (isset($_POST['color'=='Fire'])){
-                
+        if (isset($_POST['color'=='Fire'])){      
             }
         }
 
 
         ?>
 
->>>>>>> 4756803dcbb36238958acd637ed831e40a2b1e58
+>>>>>>> 
 </body>
 </html>
