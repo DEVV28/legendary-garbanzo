0bffc3feb072b07784d5f6a39d7009916d1c762a
unknown <202080469@psu.palawan.edu.ph>
2022-09-19 21:44:50 +0800

    done web3

--- web2/fav.php
+++ web2/fav.php
@@ -2,6 +2,30 @@
 <head><title>Your Favorite</title></head>
 <body>
     <?php
+    
+    if (isset($_POST['username']) && !empty($_POST['username'])){
+        $username=$_POST['username'];
+            echo "Thanks for you selection ". $username. "<br />";
+    } else {
+        echo "invalid input, please enter your username". "<br />";
+    }
+
+
+    if (isset($_POST['dish']) && !empty($_POST['dish'])){
+        $dish=$_POST['dish'];
+            echo "You really enjoyed your ". $dish. "<br />";
+    } else {
+        echo "invalid input, please enter your favorite dish". "<br />";
+    }
+
+
+    if (isset($_POST['color']) && !empty($_POST['color'])){
+        $color=$_POST['color'];
+            echo "Especially with a nice ". $color. "\n". "wine";
+    } else {
+        echo "invalid input, please select your favorite color". "<br />";
+    }
+            
         
     ?>
 </body>
